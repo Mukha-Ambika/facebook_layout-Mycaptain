8db85076cc57b8abce8bff2ee2f5ede8493c477f
Mukha-Ambika <ambika.varada@gmail.com>
2020-07-10 21:25:42 +0530

    first commit

---
+++ app/src/main/java/com/example/facebook_layoutmycaptain/Register_fb.java
@@ -0,0 +1,14 @@
+package com.example.facebook_layoutmycaptain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register_fb extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_fb);
+    }
+}
